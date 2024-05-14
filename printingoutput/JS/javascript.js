0a028a9b63f6d3294da19e3748963d04fd87ceed
rarrdeguzman <157084046+rarrdeguzman@users.noreply.github.com>
2024-05-14 18:58:28 -0400

    Add files via upload

---
+++ printingoutput/JS/javascript.js
@@ -0,0 +1,37 @@
+let a = 3;
+let b = 5;
+let c;
+
+let print =
+`
+Task 1
+a + b = ${a+b}
+a - b = ${a-b}
+a * b = ${a*b}
+a / b = ${a/b}
+a % b = ${a%b}
+a += b = ${a+=b}
+a -= b = ${a-=b}
+a *= b = ${a*=b}
+a /= b = ${a/=b}
+a %= b = ${a%=b}
+a == b = ${a==b}
+a != b = ${a!=b}
+a > b = ${a>b}
+a < b = ${a<b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}`;
+
+console.log(print);
+
+let first_name = `Romeo`;
+let last_name = `De Guzman`;
+let email = `rarrdeguzman@gmail.com`;
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+console.log(`Task 2 \n ${output}`);
+
+
+
+
+
